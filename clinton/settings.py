300db9c6a23f78ffa6ddde3abe7b94814671f02e
Julius Njeru <127553760+njeruthuo@users.noreply.github.com>
2025-02-19 07:43:15 +0300

    deployment attempt

--- clinton/settings.py
+++ clinton/settings.py
@@ -29,12 +29,13 @@ SECRET_KEY = 'django-insecure-k)i1_pqkr2fr1c_!u&07xc-b77xbegpkzs@xxmup7-kvlc0fle
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", ".vercel.app", ".now.sh", 'localhost']
 
 
 # Application definition
 
-INSTALLED_APPS = ['corsheaders', 'rest_framework', 'users', 'exams', 'rest_framework.authtoken',
+INSTALLED_APPS = ['corsheaders', 'rest_framework', 'users', 'exams', 'rest_framework.authtoken', 'cloudinary',
+                  'cloudinary_storage',
                   'django.contrib.admin',
                   'django.contrib.auth',
                   'django.contrib.contenttypes',
@@ -82,14 +83,25 @@ TEMPLATES = [
 WSGI_APPLICATION = 'clinton.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('PGDATABASE'),
+        'USER': os.environ.get('PGUSER'),
+        'PASSWORD': os.environ.get('PGPASSWORD'),
+        'HOST': os.environ.get('PGHOST'),
+        'PORT': os.environ.get('PGPORT', 5432),
+        'OPTIONS': {
+            'sslmode': 'require',
+        },
+        'DISABLE_SERVER_SIDE_CURSORS': True,
     }
+}
+
+CLOUDINARY_STORAGE = {
+    "CLOUD_NAME": os.getenv("CLOUDINARY_CLOUD_NAME"),
+    "API_KEY": os.getenv("CLOUDINARY_API_KEY"),
+    "API_SECRET": os.getenv("CLOUDINARY_API_SECRET"),
 }
 
 
@@ -133,11 +145,14 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles", 'static')
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-# STORAGES = {
-#     "staticfiles": {
-#         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-#     },
-# }
+STORAGES = {
+    "default": {
+        "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
