4e840407c57be8f624bfe7705946a02818b6b118
njeruthuo <juliusn411@gmail.com>
2025-06-05 15:22:11 +0300

    improve performance while fetching

--- db/exams/views.py
+++ db/exams/views.py
@@ -7,9 +7,12 @@ from .serializers import ExamSerializer, Exam
 
 class ExamAPIView(APIView):
     def get(self, request, *args, **kwargs):
-        exams = Exam.objects.all()
-        serializer_data = ExamSerializer(exams, many=True).data
-        return Response(serializer_data, status=status.HTTP_200_OK)
+        exams = Exam.objects.prefetch_related(
+            'questions__answers'
+        )
+
+        serializer_data = ExamSerializer(exams, many=True)
+        return Response(serializer_data.data, status=status.HTTP_200_OK)
 
     def post(self, request, *args, **kwargs):
         return Response({}, status=status.HTTP_200_OK)
